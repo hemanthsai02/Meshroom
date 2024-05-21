10e7d0f4d4ee4f6d0392d2ab08a24175c6753ea0
Fabien Servant <fabien.servant@technicolor.com>
2024-05-21 15:39:15 +0200

    add sfmExpanding

---
+++ meshroom/nodes/aliceVision/SfmExpanding.py
@@ -0,0 +1,49 @@
+__version__ = "1.0"
+
+from meshroom.core import desc
+from meshroom.core.utils import DESCRIBER_TYPES, VERBOSE_LEVEL
+
+
+class SfMExpanding(desc.AVCommandLineNode):
+    commandLine = 'aliceVision_sfmExpanding {allParams}'
+    size = desc.DynamicNodeSize('input')
+
+    category = 'Sparse Reconstruction'
+    documentation = '''
+'''
+
+    inputs = [
+        desc.File(
+            name="input",
+            label="SfMData",
+            description="SfMData file.",
+            value="",
+            uid=[0],
+        ),
+        desc.File(
+            name="tracksFilename",
+            label="Tracks File",
+            description="Tracks file.",
+            value="",
+            uid=[0],
+        ),
+        desc.ChoiceParam(
+            name="verboseLevel",
+            label="Verbose Level",
+            description="Verbosity level (fatal, error, warning, info, debug, trace).",
+            values=VERBOSE_LEVEL,
+            value="info",
+            exclusive=True,
+            uid=[],
+        ),
+    ]
+
+    outputs = [
+        desc.File(
+            name="output",
+            label="SfMData",
+            description="Path to the output SfMData file.",
+            value=desc.Node.internalFolder + "sfm.json",
+            uid=[],
+        ),
+    ]
